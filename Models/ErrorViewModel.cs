1d8d53b70e006e24e81ce3f7ffbb4c4cd2f41778
lourj <lourj@KAMY>
2024-10-15 09:18:14 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FogachoReveloProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
